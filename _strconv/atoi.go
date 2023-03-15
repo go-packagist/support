19199b11e2fa03c03be0227ebdcb56d484915fff
Flc゛ <four_leaf_clover@foxmail.com>
2023-03-15 19:17:30 +0800

    feat: add `_strconv.Atoi` (#6)

---
+++ _strconv/atoi.go
@@ -0,0 +1,39 @@
+package _strconv
+
+import "strconv"
+
+// AtoiRes is the result of Atoi.
+type AtoiRes struct {
+	val int
+	err error
+}
+
+// Val returns the value of the result.
+func (r AtoiRes) Val() int {
+	return r.val
+}
+
+// Err returns the error of the result.
+func (r AtoiRes) Err() error {
+	return r.err
+}
+
+// IsErr returns true if the result is an error.
+func (r AtoiRes) IsErr() bool {
+	return r.err != nil
+}
+
+// IsOk returns true if the result is ok.
+func (r AtoiRes) IsOk() bool {
+	return r.err == nil
+}
+
+// Atoi converts a string to an int.
+func Atoi(s string) AtoiRes {
+	val, err := strconv.Atoi(s)
+
+	return AtoiRes{
+		val: val,
+		err: err,
+	}
+}
